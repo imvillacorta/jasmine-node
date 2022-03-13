7183db840a6ec66388f2d5312c241dbda508c0f8
Ivan Villacorta <ivan_mv23@hotmail.com>
2022-03-13 18:01:18 -0300

    Configurando o Karma com o Browserify

--- spec/exemplo/exemploSpec.js
+++ spec/exemplo/exemploSpec.js
@@ -2,4 +2,8 @@ describe('Suite de teste de exemplo com NodeJs', () => {
     it('deve demostrar o uso do Jasmine com NodeJs', () => {
         expect(true).toEqual(true);
     });
+
+    it('deve demostrar o uso do karma em modo de escuta', () => {
+        expect(1).toEqual(1);
+    });
 });
